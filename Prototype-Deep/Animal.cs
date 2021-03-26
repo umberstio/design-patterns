0405471a0fd4779d946bced3ad32a58896eb73bb
umberstio <umberstio@outlook.com>
2021-03-26 18:58:41 -0300

    Prototype Deep Pattern (Simple)

---
+++ Prototype-Deep/Animal.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Prototype_Deep
+{
+    class Animal : ICloneable
+    {
+        public int Patas { get; set; }
+        public string Nombre { get; set; }
+
+        public Detalles Detalles { get; set; }
+
+        public object Clone()
+        {
+            var retorno = (Animal) this.MemberwiseClone();
+            retorno.Detalles = (Detalles) this.Detalles.Clone();
+            return retorno;
+        }
+    }
+
+
+    class Detalles : ICloneable
+    {
+        public string Color { get; set; }
+        public string Raza { get; set; }
+
+        public object Clone()
+        {
+            return (Detalles) this.MemberwiseClone();
+        }
+    }
+}
---
+++ Prototype-Deep/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Prototype_Deep
+{
+    /*
+     * PROTOTYPE DEEP:
+     * 
+     * Clona los objetos dentro de nuestro objeto
+     */
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var animal1 = new Animal
+            {
+                Patas = 4,
+                Nombre = "Animalito",
+                Detalles = new Detalles
+                {
+                    Raza  = "Cacri",
+                    Color = "Negro"
+                }
+            };
+
+            var animalClonado = (Animal)animal1.Clone();
+            animalClonado.Nombre = "se le cambio el nombre";
+            animalClonado.Detalles.Color = "le cambio el color";
+
+            Console.WriteLine("Animal 1:");
+            Console.WriteLine(animal1.Patas);
+            Console.WriteLine(animal1.Nombre); 
+            Console.WriteLine(animal1.Detalles.Color);
+
+            Console.WriteLine("\n");
+            Console.WriteLine("Animal 2:");
+            Console.WriteLine(animalClonado.Patas);
+            Console.WriteLine(animalClonado.Nombre);
+            Console.WriteLine(animalClonado.Detalles.Color);
+
+            Console.WriteLine("\n");
+            Console.WriteLine("Animal 1 again:");
+            Console.WriteLine("Animal 1:");
+            Console.WriteLine(animal1.Patas);
+            Console.WriteLine(animal1.Nombre);
+            Console.WriteLine(animal1.Detalles.Color);
+        }
+    }
+}
