95393fce40e5169fe8ee569839fee2b090d40aa2
umberstio <umberstio@outlook.com>
2021-03-26 18:40:55 -0300

    Prototype Pattern para clonado superficial

---
+++ Prototype/Animal.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Prototype
+{
+    /*
+     * Heredamos de ICLonable
+     * retornammos el objeto clonado en Clone()
+     */
+    class Animal : ICloneable
+    {
+        public int Patas { get; set; }
+        public string Nombre { get; set; }
+
+        public object Clone()
+        {
+            return this.MemberwiseClone();
+        }
+    }
+}
---
+++ Prototype/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Prototype
+{
+    class Program
+    {
+        /*
+         * PROTOTYPE:
+         * 
+         * Utilizado para clonar un objeto. ( clonacion superficial, de los atributos del objeto)
+         * 
+         */
+        static void Main(string[] args)
+        {
+            var animal1 = new Animal
+            {
+                Patas = 4,
+                Nombre = "Animalito"
+            };
+
+            var animalClonado = (Animal) animal1.Clone();
+            animalClonado.Nombre = "se le cambio el nombre";
+
+            Console.WriteLine("Animal 1:");
+            Console.WriteLine(animal1.Patas);
+            Console.WriteLine(animal1.Nombre);
+
+            Console.WriteLine("\n");
+            Console.WriteLine("Animal 2:");
+            Console.WriteLine(animalClonado.Patas);
+            Console.WriteLine(animalClonado.Nombre);
+
+            Console.WriteLine("\n");
+            Console.WriteLine("Animal 1 again:");
+            Console.WriteLine("Animal 1:");
+            Console.WriteLine(animal1.Patas);
+            Console.WriteLine(animal1.Nombre);
+
+        }
+    }
+}
