28e4e8426844809b99353ff1b76f62fbcf12e209
umberstio <umberstio@outlook.com>
2021-03-26 18:22:09 -0300

    Patron de diseño Singleton

---
+++ Singleton/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Singleton
+{
+    class Program
+    {
+        /*
+         * SINGLETON:
+         * Solución para casos de instanciación excesiva
+         * y reutilizar esta a lo largo del programa  
+         *  
+         *  DESVENTAJAS:
+         *  - Funciona de forma sincrona, por lo que la alta concurrencia podria dar problemas
+         *  - Se debe tener cuidado ya que al modificar su estado, este permanecerá cambiado
+         *  a lo alrgo del programa.
+         */
+
+        static void Main(string[] args)
+        {
+            var myObj = SingletonPattern.Instance;
+
+            Console.WriteLine(myObj.Message);
+
+            SingletonPattern.Instance.Message = 
+                "lo cambio desde afuera, ya no soy el mensaje original";
+            
+            Console.WriteLine(SingletonPattern.Instance.Message);
+        }
+    }
+}
---
+++ Singleton/SingletonPattern.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Singleton
+{
+    public class SingletonPattern
+    {
+        private static SingletonPattern instance = null;
+
+        public string Message { get; set; }
+
+        protected SingletonPattern()
+        {
+            Message = "Fui instanciado por que era null";
+        }
+
+        /// <summary>
+        /// Debe usar esta propiedad para obtener la instancia del objeto
+        /// </summary>
+        public static SingletonPattern Instance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new SingletonPattern();
+
+                return instance;
+            }
+        }
+
+    }
+}
